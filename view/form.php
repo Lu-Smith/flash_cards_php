260de781df1c8e201d6bd299075a31508362d28a
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-10-13 15:23:05 +0100

    Updated form.php

---
+++ view/form.php
@@ -0,0 +1,15 @@
+<main class="main-entry">
+<h2 id="enterNumber">Enter a number:</h2>
+<form action="<?php $_SERVER['PHP_SELF'] ?>" class="main-entry_form" method="GET">
+   <input
+   class="main-entry_input"
+   id="num"
+   name="num"
+   aria-labeledby="enterNumber"
+   type="text"
+   maxlength="2"
+   autofocus
+   require />
+   <button class="main-entry_button">Go</button>
+</form>
+</main>
